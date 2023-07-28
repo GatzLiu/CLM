437bafc5679e45aff90f361db690325724f523f8
liubingqi03 <liubingqi03@kuaishou.com>
2023-07-29 00:20:50 +0800

    add clm prertraining model-mmoe v1.994

--- clm_pretraining/train_model.py
+++ clm_pretraining/train_model.py
@@ -54,10 +54,11 @@ def train_model(para):
                 # print("limit_user_real_action=", limit_user_real_action)
                 train_batch_data.append([user, item, click, like, follow, comment, forward, longview, limit_user_real_action, real_length])
 
-            # train_batch_data = np.array(train_batch_data)
-            print ("1-type(train_batch_data)= ", type(train_batch_data))
-            train_batch_data = list(train_batch_data)
-            print ("2-type(train_batch_data)= ", type(train_batch_data))
+            train_batch_data = np.array(train_batch_data)
+            print("type(train_batch_data[:,8])= ", type(train_batch_data[:,8]))
+            train_batch_data_action_list = np.array(train_batch_data[:,8])
+            print ("type(train_batch_data_action_list)= ", type(train_batch_data_action_list))
+            
             print("train_batch_data[:3,0]=", train_batch_data[:3,0])
             print("train_batch_data[:3,8]=", train_batch_data[:3,8])
             print("type(train_batch_data[:3,0])=", type(train_batch_data[:3,0]))
@@ -68,7 +69,7 @@ def train_model(para):
                 model.loss_forward, model.loss_longview],
                 feed_dict={model.users: train_batch_data[:,0],
                             model.items: train_batch_data[:,1],
-                            model.action_list: train_batch_data[:,8],
+                            model.action_list: train_batch_data_action_list,
                             model.real_length: train_batch_data[:,9],
                             model.lable_like: train_batch_data[:,3],
                             model.lable_follow: train_batch_data[:,4],
