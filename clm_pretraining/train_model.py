bb1e6d2fa9fa3f4915aec8967b95ff599441938e
liubingqi03 <liubingqi03@kuaishou.com>
2023-07-28 15:28:54 +0800

    add clm prertraining model v1.4

--- clm_pretraining/train_model.py
+++ clm_pretraining/train_model.py
@@ -12,45 +12,38 @@ def train_model(para):
     ## Load data
     [train_data, user_num, item_num] = read_data(train_path)
     print("len(train_data)=",len(train_data), ", user_num=", user_num, ", item_num=", item_num)
-    # test_data = read_data(validation_path)[0]
+    test_data = read_data(validation_path)[0]
+    print ("test_data[0:3]=", test_data[0:3])
     # para_test = [train_data, test_data, user_num, item_num, para['TOP_K'], para['TEST_USER_BATCH']]
 
+    data = {'user_num': user_num, "item_num": item_num}
 
+    ## define the model
+    model = model_MMOE(data=data, para=para)
 
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    sess.run(tf.global_variables_initializer())
 
+    ## split the training samples into batches
+    batches = list(range(0, len(train_data), para['BATCH_SIZE']))
+    batches.append(len(train_data))
 
+    ## training iteratively
+    F1_max = 0
+    for epoch in range(para['N_EPOCH']):
+        for batch_num in range(len(batches)-1):
+            train_batch_data = []
+            for sample in range(batches[batch_num], batches[batch_num+1]):
+                (user, item, click, like, follow, comment, forward, longview, user_real_action) = train_data[sample]
+                train_batch_data.append([user, item, click, like, follow, comment, forward, longview, user_real_action])
 
-
-
-    # data = {'user_num': user_num, "item_num": item_num, "popularity": 0, "pre_train_embeddings": 0,
-    #         "graph_embeddings": 0, "sparse_propagation_matrix": 0}
-
-    # ## define the model
-    # model = model_MF(data=data, para=para)
-
-    # config = tf.ConfigProto()
-    # config.gpu_options.allow_growth = True
-    # sess = tf.Session(config=config)
-    # sess.run(tf.global_variables_initializer())
-
-    # ## split the training samples into batches
-    # batches = list(range(0, len(train_data_interaction), para['BATCH_SIZE']))
-    # batches.append(len(train_data_interaction))
-
-    # ## training iteratively
-    # F1_max = 0
-    # for epoch in range(para['N_EPOCH']):
-    #     for batch_num in range(len(batches)-1):
-    #         train_batch_data = []
-    #         for sample in range(batches[batch_num], batches[batch_num+1]):
-    #             (user, pos_item) = train_data_interaction[sample]
-    #             sample_num = 0
-    #             while sample_num < para['SAMPLE_RATE']:
-    #                 neg_item = int(random.uniform(0, item_num))
-    #                 if not (neg_item in train_data[user]):
-    #                     sample_num += 1
-    #                     train_batch_data.append([user, pos_item, neg_item])
-    #         train_batch_data = np.array(train_batch_data)
+            train_batch_data = np.array(train_batch_data)
+            print("train_batch_data[0]=", train_batch_data[0])
+            break
+        break
+            
     #         _, loss = sess.run([model.updates, model.loss],
     #                            feed_dict={model.users: train_batch_data[:,0],
     #                                       model.pos_items: train_batch_data[:,1],
