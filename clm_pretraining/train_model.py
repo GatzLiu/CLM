a1d4576463981b35e36a940f32c8b0f3eab532e2
liubingqi03 <liubingqi03@kuaishou.com>
2023-07-29 21:30:35 +0800

    add clm prertraining model-mmoe v1.9997

--- clm_pretraining/train_model.py
+++ clm_pretraining/train_model.py
@@ -69,9 +69,23 @@ def train_model(para):
             # tmp = train_batch_data[:,9:]
             # tmp = np.expand_dims(tmp,0)
             
-            _, loss, loss_like, loss_follow, loss_comment, loss_forward, loss_longview = sess.run(
-                [model.updates, model.loss, model.loss_like, model.loss_follow, model.loss_comment, 
-                model.loss_forward, model.loss_longview],
+            # _, loss, loss_like, loss_follow, loss_comment, loss_forward, loss_longview = sess.run(
+            #     [model.updates, model.loss, model.loss_like, model.loss_follow, model.loss_comment, 
+            #     model.loss_forward, model.loss_longview],
+            #     feed_dict={model.users: train_batch_data[:,0],
+            #                 model.items: train_batch_data[:,1],
+            #                 model.action_list: train_batch_data[:,9:],
+            #                 # model.action_list: tmp,
+            #                 # model.action_list: train_batch_data_action_list,
+            #                 model.real_length: train_batch_data[:,8],
+            #                 model.lable_like: train_batch_data[:,3],
+            #                 model.lable_follow: train_batch_data[:,4],
+            #                 model.lable_comment: train_batch_data[:,5],
+            #                 model.lable_forward: train_batch_data[:,6],
+            #                 model.lable_longview: train_batch_data[:,7],
+            # })
+            _, loss = sess.run(
+                [model.updates, model.loss],
                 feed_dict={model.users: train_batch_data[:,0],
                             model.items: train_batch_data[:,1],
                             model.action_list: train_batch_data[:,9:],
@@ -84,6 +98,7 @@ def train_model(para):
                             model.lable_forward: train_batch_data[:,6],
                             model.lable_longview: train_batch_data[:,7],
             })
+
         print_value([epoch + 1, loss, loss_like, loss_follow, loss_comment, loss_forward, loss_longview])
         if not loss < 10 ** 10:
             print ("ERROR, loss big, loss=", loss)
