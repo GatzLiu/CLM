71c0a87739ef4dd99224c5242980767ded1d5855
liubingqi03 <liubingqi03@kuaishou.com>
2023-08-01 23:30:30 +0800

    add clm prertraining model-mmoe v1.9999999993

--- clm_pretraining/train_model.py
+++ clm_pretraining/train_model.py
@@ -5,11 +5,10 @@ from params import DIR
 
 
 def train_model(para):
-    print("start")
     ## paths of data
     train_path = DIR + 'train_data.json'
     validation_path = DIR + 'train_data.json'
-    save_model_path = '.model_ckpt/clm_model.ckpt'
+    save_model_path = './model_ckpt/clm_model.ckpt'
     # save_embeddings_path = DIR + 'pre_train_embeddings' + str(para['EMB_DIM']) + '.json'
 
     ## Load data
