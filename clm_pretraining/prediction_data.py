a9d25a197c3a9a7a4cfe8dd14620f391b3034d3a
liubingqi03 <liubingqi03@kuaishou.com>
2023-08-02 22:30:05 +0800

    add clm prertraining model-mmoe v1.99999999999993

--- clm_pretraining/prediction_data.py
+++ clm_pretraining/prediction_data.py
@@ -130,8 +130,10 @@ def mmoe_prediction_data(para):
 
         epoch_like_pred = np.array(epoch_like_pred)
         print ("epoch_like_pred=", epoch_like_pred)
-        like_pxtr = epoch_like_pred.flatten()
+        # like_pxtr = epoch_like_pred.flatten()
+        like_pxtr = [pxtr for sub_list in batch_list for row in epoch_like_pred ]
         print ("like_pxtr[0] = ", like_pxtr[0])
+        print ("len(like_pxtr) = ", len(like_pxtr))
         print ("like_pxtr = ", like_pxtr)
 
         # generate ltr model train data
