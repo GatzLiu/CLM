3465f8deebc6f0e48856405bc8a1e8581d4e2d58
liubingqi03 <liubingqi03@kuaishou.com>
2023-08-07 18:53:22 +0800

    add clm train clmmodel v1.099994

--- clm_train_with_kuairand/params/params_CLM.py
+++ clm_train_with_kuairand/params/params_CLM.py
@@ -1,6 +1,6 @@
 from params.params_common import *
 
-LR = [0.001, 500][dataset]
+LR = [0.001, 10][dataset]
 LAMDA = [0.2, 0.01][dataset]
 
 TEST_USER_BATCH = {'Tamll': 4096, 'KuaiRand': 4096}[DATASET]
