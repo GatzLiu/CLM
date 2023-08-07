7464ad0dfb1821c8eabc89839a3c2a8a5188d11f
liubingqi03 <liubingqi03@kuaishou.com>
2023-08-07 20:47:27 +0800

    add clm train clmmodel v1.099999

--- clm_train_with_kuairand/utils.py
+++ clm_train_with_kuairand/utils.py
@@ -146,11 +146,11 @@ def ndcg_for_one_samp(ranking_xtr, ranking_ens, k):
 
     order_xtr = get_order(ranking_xtr)
     order_ens = get_order(ranking_ens)
-    print(order_xtr)
-    print(order_ens)
+    # print(order_xtr)
+    # print(order_ens)
     dcg, idcg = 0, 0
     for i in range(len(ranking_xtr[:k])):
         dcg += ranking_xtr[i] / np.log(order_ens[i] + 1) / np.log(2.0)
         idcg += ranking_xtr[i] / np.log(order_xtr[i] + 1) / np.log(2.0)
-    print(dcg, idcg)
+    # print(dcg, idcg)
     return dcg / (idcg + 1e-10)
