58b34e32a9aa335bcb69bd8c6017ae3f0734a6f8
liubingqi03 <liubingqi03@kuaishou.com>
2023-08-13 13:09:46 +0800

    add clm_pretraining_with_tenrec best epoch

--- clm_pretraining_with_tenrec/train_model.py
+++ clm_pretraining_with_tenrec/train_model.py
@@ -116,12 +116,8 @@ def train_model(para):
                                 epoch_like_pred_vali, epoch_follow_pred_vali, epoch_forward_pred_vali)
         list_auc_epoch_vali.append(list_auc_vali)
 
-        # model save
-        # if ((epoch+1) == para['BEST_EPOCH']):
-        #     print ("start save model , epoch+1=", epoch+1)
-        #     save_path = saver.save(sess, save_model_path, global_step=epoch+1)
-        #     print("model save path = ", save_path)
-        if ((epoch+1) == 50):
+        model save
+        if ((epoch+1) == 60) or ((epoch+1) == para['BEST_EPOCH']):
             print ("start save model , epoch+1=", epoch+1)
             save_path = saver.save(sess, save_model_path, global_step=epoch+1)
             print("model save path = ", save_path)
