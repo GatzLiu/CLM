04588e16ea0d5e64612d6857bd7d7f6f84ab8e97
wangmeng33 <wangmeng33@kuaishou.com>
2023-08-14 16:37:07 +0800

    add LR

--- clm_train_with_kuairand/params/params_MLP.py
+++ clm_train_with_kuairand/params/params_MLP.py
@@ -9,10 +9,12 @@ pxtr_reconstruct_weight = {'primary': 0.0, 'click': 0.0, 'multi-obj': 0.0, 'unsu
 primary_weight = {'primary': 1.0, 'click': 0.0, 'multi-obj': 0.0, 'unsuper': 0.0}[loss]
 multi_object_weight = {'primary': 0.0, 'click': 0.0, 'multi-obj': 1.0, 'unsuper': 0.0}[loss]
 pxtr_weight = [1.0, 1.0, 1.0, 1.0, 1.0]
+mode = ['LR', 'MLP'][1]
 # model save
 
 all_para = {'GPU_INDEX': GPU_INDEX, 'DATASET': DATASET, 'MODEL': MODEL, 'CANDIDATE_ITEM_LIST_LENGTH': CANDIDATE_ITEM_LIST_LENGTH, 'LR': LR, 'LAMDA': LAMDA,
             'PXTR_DIM': PXTR_DIM, 'ITEM_DIM': ITEM_DIM, 'PXTR_BINS': PXTR_BINS, 'BATCH_SIZE': BATCH_SIZE, 'PRED_BATCH_SIZE': PRED_BATCH_SIZE, 'TEST_USER_BATCH': TEST_USER_BATCH, 'N_EPOCH': N_EPOCH, 'IF_PRETRAIN': False,
             'TEST_VALIDATION': 'Validation', 'TOP_K': TOP_K,
             'OPTIMIZER': 'Adam', 'SAMPLER': 'MMOE', 'AUX_LOSS_WEIGHT': 0, 'DIR': DIR, 'pxtr_weight': pxtr_weight, 'exp_weight': exp_weight,
-            'sim_order_weight': sim_order_weight, 'pxtr_reconstruct_weight': pxtr_reconstruct_weight, 'primary_weight': primary_weight, 'multi_object_weight': multi_object_weight}
+            'sim_order_weight': sim_order_weight, 'pxtr_reconstruct_weight': pxtr_reconstruct_weight, 'primary_weight': primary_weight,
+            'multi_object_weight': multi_object_weight, "mode": mode}
