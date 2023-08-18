c8877b0f78e083498a70595763597713b447ffbd
wangmeng33 <wangmeng33@kuaishou.com>
2023-08-18 16:37:14 +0800

    clear code

--- clm_train_with_kuairand/params/params_CLM.py
+++ clm_train_with_kuairand/params/params_CLM.py
@@ -1,13 +1,11 @@
 from params.params_common import *
 
-LR = [0.001, 0.01][dataset]
-LAMDA = [0.0, 0.0][dataset]
-loss = ['primary', 'click', 'multi-obj', 'unsuper'][3]
-exp_weight = {'primary': 0.0, 'click': 1.0, 'multi-obj': 0.0, 'unsuper': 1.0}[loss]
-sim_order_weight = {'primary': 0.0, 'click': 0.0, 'multi-obj': 0.0, 'unsuper': 2.0}[loss]
-pxtr_reconstruct_weight = {'primary': 0.0, 'click': 0.0, 'multi-obj': 0.0, 'unsuper': 0.1}[loss]
-primary_weight = {'primary': 1.0, 'click': 0.0, 'multi-obj': 0.0, 'unsuper': 0.0}[loss]
-multi_object_weight = {'primary': 0.0, 'click': 0.0, 'multi-obj': 1.0, 'unsuper': 0.0}[loss]
+LR = 0.01
+exp_weight = {'primary': 0.0, 'click': 1.0, 'multi-obj': 0.0, 'unsuper': 1.0}[LOSS]
+sim_order_weight = {'primary': 0.0, 'click': 0.0, 'multi-obj': 0.0, 'unsuper': 2.0}[LOSS]
+pxtr_reconstruct_weight = {'primary': 0.0, 'click': 0.0, 'multi-obj': 0.0, 'unsuper': 0.1}[LOSS]
+primary_weight = {'primary': 1.0, 'click': 0.0, 'multi-obj': 0.0, 'unsuper': 0.0}[LOSS]
+multi_object_weight = {'primary': 0.0, 'click': 0.0, 'multi-obj': 1.0, 'unsuper': 0.0}[LOSS]
 pxtr_weight_for_ranking_sim_loss = [1.0, 1.0, 1.0, 1.0, 1.0]
 pxtr_weight_for_multi_object = [0.2, 0.3, 0.8, 3.0, 0.0]
 bias_weight = 10.0
@@ -16,10 +14,10 @@ decay = 0.5
 if_debias = [True, False][0]
 
 # model save
-all_para = {'GPU_INDEX': GPU_INDEX, 'DATASET': DATASET, 'MODEL': MODEL, 'CANDIDATE_ITEM_LIST_LENGTH': CANDIDATE_ITEM_LIST_LENGTH, 'LR': LR, 'LAMDA': LAMDA,
-            'PXTR_DIM': PXTR_DIM, 'ITEM_DIM': ITEM_DIM, 'PXTR_BINS': PXTR_BINS, 'BATCH_SIZE': BATCH_SIZE, 'PRED_BATCH_SIZE': PRED_BATCH_SIZE, 'TEST_USER_BATCH': TEST_USER_BATCH, 'N_EPOCH': N_EPOCH, 'IF_PRETRAIN': False,
-            'TEST_VALIDATION': 'Validation', 'TOP_K': TOP_K,
-            'OPTIMIZER': 'Adam', 'SAMPLER': 'MMOE', 'AUX_LOSS_WEIGHT': 0, 'DIR': DIR, 'pxtr_weight': pxtr_weight_for_ranking_sim_loss, 'exp_weight': exp_weight,
+all_para = {'GPU_INDEX': GPU_INDEX, 'DATASET': DATASET, 'MODEL': MODEL, 'CANDIDATE_ITEM_LIST_LENGTH': CANDIDATE_ITEM_LIST_LENGTH, 'LR': LR,
+            'PXTR_DIM': PXTR_DIM, 'ITEM_DIM': ITEM_DIM, 'PXTR_BINS': PXTR_BINS, 'BATCH_SIZE': BATCH_SIZE, 'PRED_BATCH_SIZE': PRED_BATCH_SIZE,
+            'TEST_USER_BATCH': TEST_USER_BATCH, 'N_EPOCH': N_EPOCH, 'TEST_VALIDATION': 'Validation', 'TOP_K': TOP_K,
+            'OPTIMIZER': 'Adam', 'DIR': DIR, 'pxtr_weight': pxtr_weight_for_ranking_sim_loss, 'exp_weight': exp_weight,
             'sim_order_weight': sim_order_weight, 'pxtr_reconstruct_weight': pxtr_reconstruct_weight, 'bias_weight': bias_weight,
-            'layer_num': layer_num, 'decay': decay, 'if_debias': if_debias, 'primary_weight': primary_weight, 'multi_object_weight': multi_object_weight,
-            'pxtr_prompt': pxtr_weight_for_multi_object}
+            'layer_num': layer_num, 'decay': decay, 'if_debias': if_debias, 'primary_weight': primary_weight,
+            'multi_object_weight': multi_object_weight, 'pxtr_prompt': pxtr_weight_for_multi_object}
--- clm_train_with_kuairand/params/params_common.py
+++ clm_train_with_kuairand/params/params_common.py
@@ -1,19 +1,15 @@
 
-
 GPU_INDEX = "0"
-dataset = 1         # 0:KuaiRand, 1:Tenrec
-model = 2
-
-DATASET = ['Tamll', 'KuaiRand'][dataset]
-MODEL = ['CLM', 'PRM', 'MLP', 'SUM', 'MUL'][model]
+MODEL = ['CLM', 'PRM', 'MLP', 'SUM', 'MUL'][0]
+LOSS = ['primary', 'click', 'multi-obj', 'unsuper'][3]
+DATASET = 'KuaiRand'
 CANDIDATE_ITEM_LIST_LENGTH = 100
 PXTR_DIM = 16
 ITEM_DIM = 64
 PXTR_BINS = 10000
-
 N_EPOCH = 50
 BATCH_SIZE = 10000
 PRED_BATCH_SIZE = 2000
-TEST_USER_BATCH = [1000, 1000][dataset]
+TEST_USER_BATCH = 1000
 TOP_K = [10, 20, 30, 40, 50]
 DIR = '../clm_pretraining_with_kuairand/dataset/'+DATASET+'/'
