da32dbb089c6d9327aaf4bfcb2e5c9e65eee5379
yuwenhui07 <yuwenhui07@kuaishou.com>
2024-01-11 19:38:33 +0800

    change LR

--- clm_train_with_kuairand/params/params_PRM.py
+++ clm_train_with_kuairand/params/params_PRM.py
@@ -1,8 +1,9 @@
 from params.params_common import *
 
-LR = 0.01
-transformer = ['transformer', 'set_transformer', 'DC2IN', 'SoGCN', 'orth_transformer'][0]
-# model save
+trans = 1
+transformer = ['transformer', 'set_transformer', 'DC2IN', 'SoGCN', 'orth_transformer'][trans]
+LR = [0.02, 0.02, 0.02, 0.02, 0.02][trans]
+LR = 0.02
 
 all_para = {'GPU_INDEX': GPU_INDEX, 'DATASET': DATASET, 'MODEL': MODEL, 'CANDIDATE_ITEM_LIST_LENGTH': CANDIDATE_ITEM_LIST_LENGTH, 'LR': LR,
             'PXTR_DIM': PXTR_DIM, 'ITEM_DIM': ITEM_DIM, 'PXTR_BINS': PXTR_BINS, 'BATCH_SIZE': BATCH_SIZE, 'PRED_BATCH_SIZE': PRED_BATCH_SIZE,
